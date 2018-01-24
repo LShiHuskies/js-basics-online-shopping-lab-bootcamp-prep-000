15b65526c9920499f5464cc7f31a7f86623e53ed
Louis Shi <louis.shi@uconn.edu>
2018-01-24 05:10:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,4 +67,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber == null) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
+  card = [];
 }
