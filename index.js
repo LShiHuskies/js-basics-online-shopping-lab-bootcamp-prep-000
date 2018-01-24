0c62978be01818ca21a218592cc4c93d8866e825
Louis Shi <louis.shi@uconn.edu>
2018-01-24 02:30:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function viewCart() {
         newarray.push(item + " at $" + cart[i][item])
       }
     }
-    console.log(mystring + newarray.join(", ") + ".")
+    console.log("In your cart, you have " + newarray.join(", ") + ".")
   }
 }
 
