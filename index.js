68a61163f1c60da5e24e75ed2d7e0d4584d1da8e
Louis Shi <louis.shi@uconn.edu>
2018-01-24 03:51:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function viewCart() {
     }
     if(newarray.length > 1) {
       var lastelement = newarray.slice(-1)[0]
-      newarray.pop() + "and " + lastelement
+      newarray.pop()
       newarray.join("and ") + lastelement
     }
     console.log(mystring + newarray.join(", ") + ".")
